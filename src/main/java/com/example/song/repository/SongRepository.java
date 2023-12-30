40f87cb3d6cdc31c96cb6775bb699b9cd428d916
pruthviraj <battipruthviraj@gmail.com>
2023-12-30 19:25:04 +0000

    first commit

---
+++ src/main/java/com/example/song/repository/SongRepository.java
@@ -0,0 +1,16 @@
+package com.example.song.repository;
+
+import java.util.*;
+import com.example.song.model.*;
+
+public interface SongRepository {
+   ArrayList<Song> getAllSongs();
+
+   Song getSongById(int songId);
+
+   Song addSong(Song song);
+
+   Song updateSong(int songId, Song song);
+
+   void deleteSong(int songId);
+}
